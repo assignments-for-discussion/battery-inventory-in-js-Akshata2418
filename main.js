0a365fedd54f37e8f5cfce22ac54061d919029f9
Akshata2418 <79439559+Akshata2418@users.noreply.github.com>
2023-10-18 16:22:30 +0530

    Update main.js
    Update main.js with functions and code to calculate SoH and categorize by SoH.

--- main.js
+++ main.js
@@ -1,20 +1,53 @@
 const assert = require('assert');
 
+//Calculate SoH by capacity
+function calculateSoH(capacity) {
+  return 100 * (capacity / 120);
+}
+
+//Classify battery health by SoH
+function classifyBatteryBySoH(capacity) {
+  const soh = calculateSoH(capacity);
+  if (soh > 80) {
+    return "healthy";
+  } else if (soh >= 63) {
+    return "exchange";
+  } else {
+    return "failed";
+  }
+}
+
+//Count the number of batteries falling under each classification.
 function countBatteriesByHealth(presentCapacities) {
-  return {
+  const counts = {
     healthy: 0,
     exchange: 0,
-    failed: 0
+    failed: 0,
   };
+
+  presentCapacities.forEach((capacity) => {
+    const classification = classifyBatteryBySoH(capacity);
+    counts[classification]++;
+  });
+  return counts;
 }
 
 function testBucketingByHealth() {
   console.log('Counting batteries by SoH...');
   const presentCapacities = [113, 116, 80, 95, 92, 70];
-  counts = countBatteriesByHealth(presentCapacities);
+  const counts = countBatteriesByHealth(presentCapacities);
   assert(counts["healthy"] == 2);
   assert(counts["exchange"] == 3);
   assert(counts["failed"] == 1);
+  console.log("Number of batteries according to SoH :", counts);
+
+  //New test data 
+  const newpresentCapacities = [108, 117, 80, 87, 95, 92, 60, 70, 107];
+  const testcounts = countBatteriesByHealth(newpresentCapacities);
+  assert(testcounts["healthy"] == 3);
+  assert(testcounts["exchange"] == 4);
+  assert(testcounts["failed"] == 2);
+  
   console.log("Done counting :)");
 }
 
